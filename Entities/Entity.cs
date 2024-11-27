c653816b7618d2d1d96122fb1220ea1d9a8da0c2
Ziggy <zigs_17@hotmail.com>
2024-11-26 19:58:25 -0800

    Remove reduntant coords

--- Entities/Entity.cs
+++ Entities/Entity.cs
@@ -3,8 +3,6 @@ using SFML.System;
 public abstract class Entity
 {
     public Vector2i Position { get; set; }
-    public int X { get; set; }
-    public int Y { get; set; }
     public abstract void Update(World world, float deltaTime);
     public abstract void Draw(Renderer renderer);
 }
